6cc785b435a4220b5aa946b642dd569295be3fe7
Sangeetha-Naresh <sangeetha.nareshm@gmail.com>
2021-05-17 15:16:31 +0530

    added rect

--- Ground.js
+++ Ground.js
@@ -6,13 +6,14 @@ class Ground {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-    //  this.image=loadImage("sprites/base.png")
+
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-     // imageMode(CENTER);
-    
-    //  image(this.image,pos.x, pos.y, this.width, this.height);
+      fill("brown")
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height)
+
     }
   };
